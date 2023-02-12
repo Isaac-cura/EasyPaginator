adc1d18fb4e58cfe91e71980d6e6203883ed21c9
José <demon.mystery@gmail.com>
2023-02-12 13:24:08 -0400

    feat: Page its defined by offset limit and count(count was added)

--- src/easy-paginator/easy-paginator.ts
+++ src/easy-paginator/easy-paginator.ts
@@ -3,16 +3,45 @@ export class EasyPaginator {
     offset: number;
     limit: number;
     count: number;
+    lastPage: number;
 
     constructor(dataSource: PaginatorDataSource = {}) {
-        dataSource = Object.assign(defaultPaginatorConfig(), dataSource)
-        const { offset, limit } = dataSource
-        this.page = this.getPageFrom(offset, limit)
+        this.init(dataSource)
     }
 
-    private getPageFrom(offset: number, limit: number) {
+    private init(dataSource) {
+        dataSource = Object.assign(defaultPaginatorConfig(), dataSource)
+        const { limit, count } = dataSource
+        this.lastPage = this.getLastPage(count, limit)
+        this.page = this.getPage(dataSource)
+    }
+
+    getPage({offset, limit, count}) {
+        const lastPage = this.getLastPage(count, limit)
+        const unFixedPage = this.getUnfixedPage(offset, limit)
+        return this.getFixedPage(unFixedPage, lastPage);
+    }
+
+    private getUnfixedPage(offset: number, limit: number) {
         const firstItemShown = offset + 1;      
         return Math.ceil(firstItemShown / limit)
+    }
+
+    private getLastPage(count: number, limit) {
+        return Math.ceil(count / limit)
+    }
+
+    private pageIsInHighLimit(page, lastPage) {
+        return page <= lastPage
+    }
+
+    private getFixedPage(page, lastPage) {        
+        if(!this.pageIsInHighLimit(page, lastPage)) {
+            return lastPage;
+        } else if(page <= 1) {
+            return 1;
+        }
+        return page;
     }
 
 }
